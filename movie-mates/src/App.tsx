35e747427d10bbfb7e268c3ff80a67424a41e154
svanlaugbirna <svanlaugbirna@gmail.com>
2023-11-26 22:02:21 +0000

    prófa að pusha og pulla

--- movie-mates/src/App.tsx
+++ movie-mates/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline text-red-500">
-        This is a tailwind test
+        Testing the branch 
       </h1>
     </>
   );
