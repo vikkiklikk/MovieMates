30b8ce3fc3e31b2e280d124034fd33826ca68021
svanlaugbirna <svanlaugbirna@gmail.com>
2023-12-02 20:04:36 +0000

    added three colors

--- movie-mates/tailwind.config.js
+++ movie-mates/tailwind.config.js
@@ -10,6 +10,9 @@ export default {
       buttonColor: 'rgba(1, 22, 39, 0.8)',
       buttonHover: '#011627',
       textColor: '#FCFCFC',
+      green: '#C4EBC8',
+      white: '#F5F5F5',
+      lightText: '#86A5D9'
     },
   },
   plugins: [],
