d8f80e0ff993c9c5cd4295571a06d91de6b6a2e5
svanlaugbirna <svanlaugbirna@gmail.com>
2023-12-02 00:03:33 +0000

    added few more sections and the logo

--- movie-mates/src/pages/SignUp.tsx
+++ movie-mates/src/pages/SignUp.tsx
@@ -1,24 +1,45 @@
 import { useState } from "react";
+import Logo from "../assets/Logo - w text - light.svg";
+import BigButton from "../components/ui/BigButton";
 
 function SignUp() {
-    const [name, setName] = useState('prufa');
+  const [name, setName] = useState('prufa');
+  const buttons = [
+    { title: "Sign Up" },
+  ];
 
-    return (
-<div className="min-h-screen flex flex-col justify-center">
-
-<div className="max-w-md w-full mx-auto text-[20px]">Text outside the box</div>
-
-<div clasName="mx-auto w-[320px] h-[50px]"> 
-<form action="" className="space-y-6">
-    <div>
-        <label htmlFor="" className="text-[20px] ">Email</label>
-        <input type="text" className="w-full p-2 border bg-greenBox"/>
+  return (
+    <div className="min-h-screen flex flex-col justify-center">
+      <div className="mx-auto w-[320px] h-[50px]">
+        <div className=" bg-buttonColor w-[234] h-[209]"> <img src={Logo} alt="Logo"/></div>
+        <form action="" className="space-y-6">
+          <div>
+            <label htmlFor="" className="text-[20px] ">
+              Email
+            </label>
+            <input type="text" className="w-full p-2 border bg-greenBox" />
+          </div>
+          <div>
+            <label htmlFor="" className="text-[20px] ">
+              Password
+            </label>
+            <input type="text" className="w-full p-2 border bg-greenBox" />
+          </div>
+          <div>
+            <label htmlFor="" className="text-[20px] ">
+              Username
+            </label>
+            <input type="text" className="w-full p-2 border bg-greenBox" />
+          </div>
+          <div>
+            {buttons.map((button) => (
+              <BigButton title={button.title} />
+            ))}
+          </div>
+        </form>
+      </div>
     </div>
-</form>
- </div>
+  );
+}
 
-</div>
-    );
-};
-
-export default SignUp
+export default SignUp;
