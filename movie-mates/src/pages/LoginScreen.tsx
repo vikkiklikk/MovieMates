79f7977a4cffaecc63b96a665116c1237f99a3a4
svanlaugbirna <svanlaugbirna@gmail.com>
2023-12-03 14:49:28 +0000

    the placement is not working for me

--- movie-mates/src/pages/LoginScreen.tsx
+++ movie-mates/src/pages/LoginScreen.tsx
@@ -1,12 +1,62 @@
 import React, { useState } from "react";
+import BigButton from "../components/ui/BigButton";
+import Logo from "../assets/Logo - w text - light.svg"
 
 function Login() {
   const [name, setName] = useState('login');
+  const buttons = [
+  {title: "Login"},]
 
   return (
-    <div className="min-h-screen bg-lighBackground flex flex-col justify-center">
-      <div className="max-w-md mx-auto">text outside</div>
-      <div className="max-w-md mx-auto mt-4 p-8 border  bg-white">box 
+    <div className="flex items-center justify-center h-screen flex-col">
+      <div> 
+        <p className="font-bold"> log into your account</p>
+      </div>
+    <div className=" bg-buttonColor  w-[234px] h-[209px] rounded-[66px]">
+    <img src={Logo} alt="Logo" className=" justify-center m-auto mt-5"/>
+      </div>
+    
+
+      <div>  
+      <form action="" className="space-y-6">
+        
+      <div className="flex flex-col">
+  <label htmlFor="text" className="mb-1">email</label>
+  <input
+    type="email"
+    id="email"
+    className="w-[320px] h-[50px] rounded-2xl bg-green"
+  />
+</div>
+
+
+        <div className="flex flex-col">
+  <label htmlFor="password" className="mb-1">Password</label>
+  <input
+    type="password"
+    id="password"
+    className="w-[320px] h-[50px] rounded-2xl bg-green"
+  />
+</div>
+
+
+        <div className="flex items-center justify-between">
+          <div className="flex items-center">
+            <input type="checkbox" className="h-4 w-4"></input>
+            <label htmlFor="" className="ml-2"> Keep me signed in</label>
+          </div>
+       </div>
+{/* button */}
+<div>
+<div className="absolute bottom-7 underline text-[14px] justify-center">
+  {buttons.map((button) => (
+    <BigButton title={button.title} key={button.title} />
+  ))}
+<div> <a href="" className="underline" > Forgot your password?</a></div>
+</div>
+</div>
+
+      </form>
       </div>
     </div>
   );
