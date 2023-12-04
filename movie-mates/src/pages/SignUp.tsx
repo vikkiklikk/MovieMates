18244a8522102c06c4b4cc3305f964ff55bc6797
svanlaugbirna <svanlaugbirna@gmail.com>
2023-12-04 21:37:16 +0000

    finitotototo

--- movie-mates/src/pages/SignUp.tsx
+++ movie-mates/src/pages/SignUp.tsx
@@ -1,45 +1,54 @@
 import { useState } from "react";
 import Logo from "../assets/Logo - w text - light.svg";
 import BigButton from "../components/ui/BigButton";
+import { FaArrowLeft } from "react-icons/fa"; 
 
 function SignUp() {
   const [name, setName] = useState('prufa');
-  const buttons = [
-    { title: "Sign Up" },
-  ];
+
 
   return (
-    <div className="min-h-screen flex flex-col justify-center">
-        <div className="flex flex-col justify-center items-center"><p className="text-[20px] font-bold text-center ">create account</p></div>
-      <div className="flex items-center justify-center h-screen flex-col">
-    <div className="bg-buttonColor w-[234px] h-[209px] rounded-[66px]">
+    <div>
+      <div className="pt-7 pl-7"> <FaArrowLeft/> </div>
+        <div className="flex flex-col justify-center items-center"><p className="text-[20px] font-bold text-center mt-10 ">create account</p></div>
+      <div className="flex items-center justify-center flex-col">
+    <div className="bg-buttonColor w-[234px] h-[209px] rounded-[66px] mt-5 mb-5">
     {/* Logo with alt text */}
     <div className="flex flex-col justify-center items-center"><img src={Logo} alt="Logo" className=" justify-center m-auto mt-5"/></div>
 </div>
 <div className="items-center justify-center flex flex-col">
         <form action="" className="space-y-6" mt-4>
-          <div>
-            <label htmlFor="email" className="text-[20px] ">
-              email
-            </label>
-            <input type="text" className="bg-greenBox rounded-xl h-[50px] w-[320px]" />
-          </div>
-          <div>
-            <label htmlFor="password" className="text-[20px] ">
-              password
-            </label>
-            <input type="text" className="bg-greenBox rounded-xl h-[50px] w-[320px]" />
-          </div>
-          <div>
-            <label htmlFor="username" className="text-[20px] ">
-              username
-            </label>
-            <input type="text" className=" bg-greenBox rounded-xl h-[50px] w-[320px]" />
-          </div>
+         
+        <div className="flex flex-col">
+  <label htmlFor="text" className="mb-1">email</label>
+  <input
+    type="email"
+    id="email"
+    className="w-[350px] h-[50px] rounded-2xl bg-greenBox"
+  />
+</div>
+
+          <div className="flex flex-col">
+  <label htmlFor="password" className="mb-1">password</label>
+  <input
+    type="password"
+    id="password"
+    className="w-[350px] h-[50px] rounded-2xl bg-greenBox"
+  />
+</div>
+
+          <div className="flex flex-col">
+  <label htmlFor="text" className="mb-1">username</label>
+  <input
+    type="username"
+    id="username"
+    className="w-[350px] h-[50px] rounded-2xl bg-greenBox"
+  />
+</div>
         
-            {buttons.map((button) => (
-              <div className="justify-center w-full flex items-center "> <BigButton title={button.title} /></div>
-            ))}
+            
+              <div className="justify-center pt-10"> <BigButton title="Sign Up" /></div>
+        
         </form>
         </div>
       </div>
