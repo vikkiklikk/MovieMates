9cedd7974d836d64eed93960bd0454a0e0502261
BjorkBjorgulfs <bogga1387@gmail.com>
2023-12-05 07:18:23 +0000

    routings, started on ShowtimesPage, not finished

--- movie-mates/src/components/Footer.tsx
+++ movie-mates/src/components/Footer.tsx
@@ -2,11 +2,18 @@ import React from "react";
 import { FaRegUser } from "react-icons/fa6";
 import { TbMovie } from "react-icons/tb";
 import { IoTicketOutline, IoStatsChartOutline } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
 
 const Footer = () => {
+
+  const navigate = useNavigate();
+  const goToHomePage = () => {
+    navigate ('/');
+  };
+
   return (
     <div className="fixed bottom-0 left-0 w-screen h-[58px] bg-[#202A25] bg-opacity-95 p-2 flex justify-evenly items-center">
-      <TbMovie size={36} color="white" />
+      <TbMovie size={36} color="white" onClick={goToHomePage} />
       <IoStatsChartOutline size={30} color="white" />
       <IoTicketOutline size={30} color="white" />
       <FaRegUser size={30} color="white" />
--- movie-mates/src/components/ShowtimesPage.tsx
+++ movie-mates/src/components/ShowtimesPage.tsx
@@ -1,33 +1,64 @@
-import BoxShowtime from "./BoxShowtime";
 import {FaArrowLeft} from "react-icons/fa6";
 import { useNavigate } from 'react-router-dom';
+import TheaterContainer from "./TheaterContainer";
+import Footer from "./Footer";
+import SmallButton from "./ui/SmallButton";
+import { useState } from "react";
+
+
 
 const ShowtimesPage: React.FC = () => {
 
-    const showtimes = [
-        {time:"18:00", room: "Salur 1"},
-        {time:"20:00", room: "Salur 1"},
-        {time:"22:00", room: "Lúxus VIP"},
-        {time:"22:20", room: "Salur 2"},
-    ];
+    const [selectedShowtime, setSelectedShowtime] = useState<Showtime | null>(null);
 
     const navigate = useNavigate ();
     const handleBackClick = () => {
         navigate (-1);
+    };
+
+    const navigateToBuyTickets = () => {
+        if (selectedShowtime) {
+            navigate(`/buy-tickets/${selectedShowtime.time}/${selectedShowtime.room}`);
+            // Or use state to pass the selectedShowtime object
+        } else {
+            // Handle the case where no showtime is selected
+        }
+    };
+
+    const Buttons = [
+        {title: "Send to vote",
+        onclick: () => navigate('/')
+    },
+        {title: "Buy tickets",
+        onclick: navigateToBuyTickets
     }
+    ];
+
+    const handleSelectedShowtime = (time: string, room:string) => {
+        setSelectedShowtime({ time, room});
+    };
 
     return (
         <div>
-            <FaArrowLeft onClick={handleBackClick} />
-            {showtimes.map((showtime)=>{
-                return ( <BoxShowtime 
-                    
-                    time={showtime.time} 
-                    room={showtime.room}/>
-                );
-            })}
-            
-        <p>testing</p>
+            <div className="pb-16 pt-7 px-7">
+                <div>
+                    <FaArrowLeft onClick={handleBackClick} />
+                </div>
+                <div className="flex justify-center text-xl font-bold ">
+                    <p>Title of the movie</p>
+                </div>
+                <div className="flex justify-center">
+                    <p>here come the dates</p>
+                </div>
+                
+                <TheaterContainer onSelectedShowtime={handleSelectedShowtime} />
+                <div className="flex justify-between">
+                    {Buttons.map((button)=>{
+                        return <SmallButton key={button.title} title={button.title} onClick={button.onclick} ></SmallButton>
+                    })}
+                </div>
+            </div>
+            <Footer/>
         </div>
 
     );
