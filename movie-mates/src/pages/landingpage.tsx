57cd802913d052491fe60644ac4681cd5362fc6c
BjorkBjorgulfs <bogga1387@gmail.com>
2023-12-05 12:22:21 +0000

    finished all routings for pages that we have

--- movie-mates/src/pages/landingpage.tsx
+++ movie-mates/src/pages/landingpage.tsx
@@ -1,35 +1,50 @@
 import BigButton from "../components/ui/BigButton";
 import Logo from "../assets/Logo - w text - light.svg"; 
-
+import { useNavigate } from "react-router-dom";
 
 const LandingPage = () => {
-const buttons = [
-  { title: "Register" },
-  { title: "Take a look" }
-];
+  const navitage = useNavigate();
 
+  const goToSignUp = () => {
+    navitage('/signup');
+  };
 
-return (
-  <div className="flex items-center justify-center h-screen flex-col">
-    <div className="bg-buttonColor w-[234px] h-[209px] rounded-[66px]">
-    {/* Logo with alt text */}
-    <img src={Logo} alt="Logo" className=" justify-center m-auto mt-5"/>
-</div>
-    {/* Text and buttons under the logo */}
-    <div className="flex flex-col items-center">
-      <h1 className="font-bold text-[20px] text-center mt-10">Discover movies and share with friends!</h1>
-      <p className="mt-6 mb-10 text-[14px]" >Vote for a movie tonight</p>
+  const goToHome = () => {
+    navitage('/home');
+  };
 
-      {/* Buttons */}
-      {buttons.map((button) => (
-       <div className="item-center absolute bottom-7"> <BigButton title={button.title} key={button.title}/></div>
-      ))}
-      
-      <p className="absolute bottom-7 underline text-[14px] ">Already a member?</p>
+  const goToLogIn = () => {
+    navitage('/login');
+  };
+
+  const buttons = [
+    { title: "Register", onClick: goToSignUp },
+    { title: "Take a look", onClick: goToHome }
+  ];
+
+  return (
+    <div className="flex items-center justify-center h-screen flex-col">
+      <div className="bg-buttonColor w-[234px] h-[209px] rounded-[66px]">
+        {/* Logo with alt text */}
+        <img src={Logo} alt="Logo" className=" justify-center m-auto mt-5"/>
+      </div>
+      {/* Text and buttons under the logo */}
+      <div className="flex flex-col items-center">
+        <h1 className="font-bold text-[20px] text-center mt-10">Discover movies and share with friends!</h1>
+        <p className="mt-6 mb-10 text-[14px]" >Vote for a movie tonight</p>
+
+        {/* Buttons */}
+        {buttons.map((button) => (
+          <div className="item-center pt-3" key={button.title}> 
+            <BigButton title={button.title} onClick={button.onClick}/>
+          </div>
+        ))}
+        
+        <p className="absolute bottom-7 underline text-[14px]" onClick={goToLogIn}>Already a member?</p>
+      </div>
     </div>
-  </div>
-);
-      }
+  );
+}
 
 
 export default LandingPage;
