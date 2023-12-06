15cf1df75299529cd2797ab0243fdbcc833c040b
svanlaugbirna <svanlaugbirna@gmail.com>
2023-12-06 20:41:30 +0000

    needs the movies he likes and the footer

--- movie-mates/src/pages/Profile.tsx
+++ movie-mates/src/pages/Profile.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import 
+import UserV from "../assets/User - Viktor.svg";
+import { FaArrowLeft } from "react-icons/fa";
+import Footer from '../components/Footer';
 
 const styles = {
   ImageContainer: {
@@ -16,23 +18,49 @@ const styles = {
 };
 
 const defaultProps = {
-  image: 
+  image: UserV,
+};
 
-const Image = (props) => {
-    return (
-      <div className="">
-        <p>Your Profile</p>
-        <div
-          style={{
-            ...styles.ImageContainer,
-            backgroundImage: `url(${props.image ?? defaultProps.image})`,
-          }}
-        >
-          {/* Add any content or child components for the image container if needed */}
-        </div>
+const Image: React.FC<ImageType> = (props) => {
+  return (
+    <div className="mt-7 ml-7">
+      <div className="pt-7 pl-7">
+        <FaArrowLeft />
       </div>
-    );
-  };
-  
-  export default Image;
-  
+      <div className="justify-center flex mt-7 mb-7">
+        <p>Your Profile</p>
+      </div>
+      <div>
+        <img src={UserV} className="ml-1.5 w-[70px] h-[70px]" alt="User" />
+      </div>
+      <div>
+        <p>Viktor Birgisson</p>
+      </div>
+      <div className="bg-buttonColor text-white rounded-xl mt-5 mr-7 p-2">
+        <p>
+          DJ. Web Developer and avid movie goer. I like indie flicks and cult
+          classics. Werner Herzog, David Fincher and Tarantino are some of my
+          all-time favorite directors.
+        </p>
+      </div>
+
+      <div className="justify-center flex mt-10">
+        <h1>Liked movies</h1>
+      </div>
+
+      <div
+        style={{
+          ...styles.ImageContainer,
+          backgroundImage: `url(${props.image ?? defaultProps.image})`,
+        }}
+      >
+        {/* Add any content or child components for the image container if needed */}
+      </div>
+{/*Footer*/}
+     
+    </div>
+  );
+};
+
+export default Image;
+
