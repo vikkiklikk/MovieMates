2407047e549fffd724e92dc2504c93f0349bc071
Viktor Birgisson <viktor.birgisson@gmail.com>
2023-12-07 23:08:21 +0000

    deleted unused duplicates of svgs outside the src folder - deleted unused files

--- movie-mates/tailwind.config.js
+++ movie-mates/tailwind.config.js
@@ -1,26 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
     colors: {
-      buttonColor: 'rgba(1, 22, 39, 0.8)',
-      buttonHover: '#011627',
-      textColor: '#FCFCFC',
-      green: '#C4EBC8',
-      lighBackground: '#F5F5F5',
-      lightText: '#86A5D9',
+      buttonColor: "rgba(1, 22, 39, 0.8)",
+      buttonHover: "#011627",
+      textColor: "#FCFCFC",
+      green: "#C4EBC8",
+      lighBackground: "#F5F5F5",
+      lightText: "#86A5D9",
       lightGrey: '"D9D9D9',
-      greenBox: '#C4EBC8',
-      textColorDark: '#282828',
-      dropDown: '#C4EBC8',
-      lightText: '#86A5D9',
-      white: '#F5F5F5',
-      boxHover: 'rgba(234, 113, 1, 0.51)'
+      greenBox: "#C4EBC8",
+      textColorDark: "#282828",
+      dropDown: "#C4EBC8",
+      white: "#F5F5F5",
+      boxHover: "rgba(234, 113, 1, 0.51)",
     },
   },
   plugins: [],
-}
+};
