e41275b02bb69f067331207f7c988602d8f6307c
BjorkBjorgulfs <bogga1387@gmail.com>
2023-12-08 09:06:29 +0000

    fixed view of moviecard

--- movie-mates/src/components/ui/MovieCard.tsx
+++ movie-mates/src/components/ui/MovieCard.tsx
@@ -28,7 +28,7 @@ const MovieCard: React.FC<MovieCardProps> = ({movie }) => {
     }
 
     return (
-        <div className={`w-[10.5rem] h-[15.5rem] rounded-2xl border p-1`}>
+        <div className={`w-[10.5rem] h-[15.5rem] rounded-2xl border p-1 pt-3`}>
             <div className={`flex`}>
                 <button className={`place-self-start relative inline-block`} onClick={handleFavoriteClick}>
                     {/*I'm using two stars that overlay each other, couldn't find a star that I could change fill and border */}
@@ -42,11 +42,11 @@ const MovieCard: React.FC<MovieCardProps> = ({movie }) => {
                 </Link>
             </div>
             <Link to={`/movie/${movie.title}`}>
-                <div className={`flex flex-col gap-y-0.5 py-1`}>
+                <div className={`flex flex-col gap-y-0.5 py-1 mt-4 mx-2`}>
                     <h2 className={`text-sm font-semibold whitespace-nowrap overflow-hidden overflow-ellipsis`}>{movie.title}</h2>
                     <p className={`text-[0.5rem]`}>{movie.genres.join(',')}</p>
-                    <p className={`text-xs whitespace-nowrap overflow-hidden overflow-ellipsis`}>Director: {movie.director}</p>
-                    <p className={`text-xs whitespace-nowrap overflow-hidden overflow-ellipsis`}>Starring: {movie.cast.join(',')}</p>
+                    {/*<p className={`text-xs whitespace-nowrap overflow-hidden overflow-ellipsis`}>Director: {movie.director}</p>
+                    <p className={`text-xs whitespace-nowrap overflow-hidden overflow-ellipsis`}>Starring: {movie.cast.join(',')}</p>*/}
                 </div>
             </Link>
         </div>
